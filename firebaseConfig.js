1d6960a92089169b480b7b5ab798e23cb15ebf0d
random56 <50276566+random56@users.noreply.github.com>
2024-02-07 21:03:56 +0100

    Updated auth data firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAqfKrWkemEdjzj1ar8ZmDtSrvvq7ClXCQ",
-  authDomain: "balance-40f00.firebaseapp.com",
-  projectId: "balance-40f00",
-  storageBucket: "balance-40f00.appspot.com",
-  messagingSenderId: "108809610562",
-  appId: "1:108809610562:web:4eba752c6b3ae6497b94db",
-  measurementId: "G-FDYBGBK8P3"
+  apiKey: "AIzaSyAbayEsSI9NgzFiRI8WfDxDvuGzl2w7or4",
+  authDomain: "firecode-56ca4.firebaseapp.com",
+  projectId: "firecode-56ca4",
+  storageBucket: "firecode-56ca4.appspot.com",
+  messagingSenderId: "398013977867",
+  appId: "1:398013977867:web:ef0f05631dc74360137e0e",
+  measurementId: "G-0R1FMX49FJ"
 };
 
 // Initialize Firebase
